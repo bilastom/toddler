1ba8314247f11d38532b6813c50be47927ae07e2
Tomasz Biłas <tomasz.bilas@gmail.com>
2019-01-31 18:40:47 +0100

    add auth navigator, set default app screen

---
+++ screens/SignInScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native'
+
+export default class SignInScreen extends React.Component {
+  render(){
+    return(
+      <View>
+        <Text>I'm SignIn screen!</Text>
+      </View>
+    )
+  }
+}
